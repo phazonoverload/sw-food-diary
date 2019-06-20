4f88b024e32b26e2373d4d444496be1ec2fbddb6
Kevin Lewis <kvn@lws.io>
2019-06-20 18:31:01 +0100

    Scaffing pages

---
+++ pages/record/_day.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Date.</h1>
+  </div>
+</template>
---
+++ pages/record/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="page">
+    <div class="content">
+      <h1>Today.</h1>
+    </div>
+    <Nav/>
+  </div>
+</template>
+
+<script>
+import Nav from "~/components/Nav";
+export default {
+  components: {
+    Nav
+  }
+};
+</script>
+
+<style scoped>
+#page {
+  min-height: calc(100vh - 2em);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
