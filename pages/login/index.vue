2313ea32713920f510483be2a39c353a78cd14d5
Kevin Lewis <kvn@lws.io>
2019-06-21 11:18:09 +0100

    Auth pages styled

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
     <h1 id="title">Login.</h1>
-    <n-link to="/record">Assume login successful</n-link>
+    <form @submit.prevent="login">
+      <input type="email" placeholder="Email Address" v-model="email">
+      <input type="password" placeholder="Password" v-model="password">
+      <input type="submit" value="Login">
+    </form>
+    <n-link to="/register">Create a new account</n-link>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {}
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin-bottom: 2em;
+}
+</style>
---
+++ pages/register/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 id="title">Register.</h1>
+    <form @submit.prevent="register">
+      <input type="email" placeholder="Email Address" v-model="email">
+      <input type="password" placeholder="Password" v-model="password">
+      <input type="submit" value="Create Account">
+    </form>
+    <n-link to="/login">Login to existing account</n-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    register() {}
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin-bottom: 2em;
+}
+</style>
